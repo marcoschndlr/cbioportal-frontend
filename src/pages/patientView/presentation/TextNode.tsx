2022ef94669d141f8e320d9cf0c8c92e2236f724
Marco Schindler <marco.schindler@fau.de>
2024-06-17 16:23:37 +0200

    add basic drag with dnd-kit and edit

--- src/pages/patientView/presentation/TextNode.tsx
+++ src/pages/patientView/presentation/TextNode.tsx
@@ -1,145 +1,113 @@
-import React from 'react';
-import { boundMethod } from 'autobind-decorator';
-
-interface Position {
-    y: number;
-    x: number;
-}
+import React, { RefObject, useEffect } from 'react';
+import { useDraggable } from '@dnd-kit/core';
 
 interface State {
-    position: Position;
     down: boolean;
     selected: boolean;
     editing: boolean;
 }
 
-class TextNode extends React.Component<{}, State> {
-    ref = React.createRef<HTMLDivElement>();
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            down: false,
-            selected: false,
-            editing: false,
-            position: {
-                x: 0,
-                y: 0,
-            },
-        };
-
-        document.addEventListener('pointermove', this.onPointerMove);
-        document.addEventListener('pointerup', this.onPointerUp);
-        document.addEventListener('keydown', this.onKeyDown);
-        document.addEventListener('click', this.onOutsideClick);
-    }
-
-    focus() {
-        this.ref.current?.focus();
-        document.getSelection()!.collapse(this.ref.current, 1);
-    }
-
-    @boundMethod
-    onPointerDown(event: React.PointerEvent) {
-        this.setState({ ...this.state, down: true, selected: true });
-    }
-
-    @boundMethod
-    onPointerUp(event: PointerEvent) {
-        this.setState({ ...this.state, down: false });
-    }
-
-    @boundMethod
-    onPointerMove(event: PointerEvent) {
-        if (this.state.editing) {
-            return;
-        }
-        const canvas = document.querySelector('.presentation');
-
-        if (this.state.down) {
-            const { y, x } = this.state.position;
-            this.setState({
-                ...this.state,
-                position: {
-                    x: x + event.movementX,
-                    y: y + event.movementY,
-                },
-            });
-        }
-    }
-
-    @boundMethod
-    onOutsideClick(event: MouseEvent) {
-        if (
-            event.target !== this.ref.current &&
-            !this.ref.current?.contains(event.target as Node)
-        ) {
-            this.handleEscapePress();
-        }
-    }
-
-    unselectNode() {
-        this.setState({ ...this.state, selected: false });
-    }
-
-    @boundMethod
-    startEditing() {
-        this.setState({ ...this.state, editing: true });
-        setTimeout(() => this.focus());
-    }
-
-    stopEditing() {
-        this.setState({ ...this.state, editing: false });
-    }
-
-    @boundMethod
-    onKeyDown(event: KeyboardEvent) {
-        switch (event.code) {
-            case 'Escape':
-                this.handleEscapePress();
-                break;
-            case 'Backspace':
-                this.ref.current?.remove();
-                break;
-        }
-    }
-
-    render() {
-        return (
-            <div
-                ref={this.ref}
-                style={this.calculateTransformation()}
-                className={`presentation__text-node 
-                     ${
-                         this.state.selected
-                             ? 'presentation__node--selected'
-                             : ''
-                     } 
-                     ${this.state.editing ? 'presentation__node--editing' : ''}
-                `}
-                onDoubleClick={this.startEditing}
-                onPointerDown={this.onPointerDown}
-                contentEditable={this.state.editing}
-                suppressContentEditableWarning={true}
-            >
-                TextNode
-            </div>
-        );
-    }
-
-    private handleEscapePress() {
-        if (this.state.editing) {
-            this.stopEditing();
-        } else {
-            this.unselectNode();
-        }
-    }
-
-    private calculateTransformation() {
-        const { x, y } = this.state.position;
-
-        return { transform: `translateX(${x}px) translateY(${y}px)` };
-    }
+interface Props {
+    innerRef: RefObject<HTMLDivElement>;
 }
 
-export default TextNode;
+export const TextNode = ({ innerRef }: Props) => {
+    const [state, setState] = React.useState<State>({
+        down: false,
+        selected: false,
+        editing: false,
+    });
+
+    useEffect(() => {
+        const controller = new AbortController();
+        (document as any).addEventListener('pointerup', onPointerUp, {
+            signal: controller.signal,
+        });
+        (document as any).addEventListener('keydown', onKeyDown, {
+            signal: controller.signal,
+        });
+        (document as any).addEventListener('pointerdown', onOutsideClick, {
+            signal: controller.signal,
+        });
+
+        return () => {
+            controller.abort();
+        };
+    }, [state]);
+
+    const focus = () => {
+        if (!innerRef) return;
+        innerRef.current?.focus();
+        document.getSelection()!.collapse(innerRef.current, 1);
+    };
+
+    const startEditing = () => {
+        setState(current => ({ ...current, editing: true }));
+        setTimeout(() => focus());
+    };
+
+    const onPointerDown = (event: React.PointerEvent) => {
+        setState(current => ({ ...current, down: true, selected: true }));
+    };
+
+    const onKeyDown = (event: KeyboardEvent) => {
+        switch (event.code) {
+            case 'Escape':
+                handleEscapePress();
+                break;
+            case 'Backspace':
+                handleBackspacePress();
+                break;
+        }
+    };
+
+    const handleEscapePress = () => {
+        if (state.editing) {
+            stopEditing();
+        } else {
+            unselectNode();
+        }
+    };
+
+    const handleBackspacePress = () => {
+        if (state.editing) return;
+        innerRef.current?.remove();
+    };
+
+    const stopEditing = () => {
+        setState(current => ({ ...current, editing: false }));
+    };
+
+    const unselectNode = () => {
+        setState(current => ({ ...current, selected: false }));
+    };
+
+    const onPointerUp = (event: PointerEvent) => {
+        setState(current => ({ ...current, down: false }));
+    };
+
+    const onOutsideClick = (event: MouseEvent) => {
+        if (
+            event.target !== innerRef.current &&
+            !innerRef.current?.contains(event.target as Node)
+        ) {
+            handleEscapePress();
+        }
+    };
+
+    return (
+        <div
+            className={`presentation__text-node 
+                     ${state.selected ? 'presentation__node--selected' : ''} 
+                     ${state.editing ? 'presentation__node--editing' : ''}
+                `}
+            ref={innerRef}
+            onDoubleClick={startEditing}
+            onPointerDown={onPointerDown}
+            contentEditable={state.editing}
+            suppressContentEditableWarning={true}
+        >
+            TextNode
+        </div>
+    );
+};
